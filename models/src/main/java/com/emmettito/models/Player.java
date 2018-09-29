5ada5f15886ea2d079f94fb5b920721e6e07611c
adow2 <adow0331@gmail.com>
2018-09-29 11:42:21 -0600

    added constructors to the player and user models

--- models/src/main/java/com/emmettito/models/Player.java
+++ models/src/main/java/com/emmettito/models/Player.java
@@ -4,6 +4,10 @@ public class Player {
     private String name;
     private int points;
 
+    public Player(String name){
+        this.name = name;
+        this.points = 0;
+    }
     public int getPoints() {
         return points;
     }
