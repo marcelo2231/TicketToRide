45e2cfe2aa11889edab797dd7c52db0cdf3df81c
Emmett <dgeough@gmail.com>
2018-10-05 12:24:28 -0600

    Added singleton client data; disabled back button on Game and GameRoom Activirito4ufeirf

--- app/src/main/java/com/emmettito/tickettoride/Client.java
+++ app/src/main/java/com/emmettito/tickettoride/Client.java
@@ -1,5 +1,55 @@
 package com.emmettito.tickettoride;
 
-public class Client{
+import com.emmettito.models.AuthToken;
+import com.emmettito.models.User;
 
+public class Client {
+
+    private static Client client = null;
+
+    String token;
+
+    String user;
+
+    String gameName;
+
+    private Client()
+    {
+        token = "";
+        user = "";
+        gameName = "";
+    }
+
+    public static Client getInstance()
+    {
+        if (client == null) {
+            client = new Client();
+        }
+
+        return client;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getGameName() {
+        return gameName;
+    }
+
+    public void setGameName(String gameName) {
+        this.gameName = gameName;
+    }
 }
