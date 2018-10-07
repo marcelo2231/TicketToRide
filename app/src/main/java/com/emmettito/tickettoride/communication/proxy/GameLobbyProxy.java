fe0a46cfc9ad3f1cdcb817f747ef526b5600f27b
Marcelo Archiza <archiza@hotmail.com>
2018-10-06 21:14:38 -0600

    Fixed client bug.

--- app/src/main/java/com/emmettito/tickettoride/communication/proxy/GameLobbyProxy.java
+++ app/src/main/java/com/emmettito/tickettoride/communication/proxy/GameLobbyProxy.java
@@ -26,9 +26,9 @@ public class GameLobbyProxy {
     }
 
     public GameLobbyResult joinGame(JoinGameRequest request) {
+        String requestString = gson.toJson(request);
         String url = "http://" + serverHost + ":" + serverPort + "/gamelobby/joingame";
-
-        return sendRequest(url, "", "GET");
+        return sendRequest(url, requestString, "GET");
     }
 
     private GameLobbyResult sendRequest(String url, String requestString, String requestType) {
