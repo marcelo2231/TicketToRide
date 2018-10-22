467050688a3a4d22f9efd849504eb0ce43f8bf92
Marcelo Archiza <archiza@hotmail.com>
2018-10-22 12:43:42 -0600

    deckDao set up

--- TicketToRideServer/src/main/java/com/emmettito/tickettorideserver/database/DeckDao.java
+++ TicketToRideServer/src/main/java/com/emmettito/tickettorideserver/database/DeckDao.java
@@ -1,6 +1,35 @@
 package com.emmettito.tickettorideserver.database;
 
+import com.emmettito.models.Cards.DestinationCard;
+import com.emmettito.models.Cards.TrainCard;
+import com.emmettito.models.Cards.TrainColor;
+
 public class DeckDao {
     /** Variables **/
     private static Database dbInstance = Database.getInstance();
+
+    public boolean addDestCard(String gameName){
+
+        return false;
+    }
+    public DestinationCard removeDestCard(String gameName){
+
+        return new DestinationCard();
+    }
+    public DestinationCard getDestCard(String gameName){
+
+        return new DestinationCard();
+    }
+    public boolean addTrainCard(String gameName){
+
+        return false;
+    }
+    public TrainCard removeTrainCard(String gameName){
+
+        return new TrainCard(TrainColor.Black);
+    }
+    public TrainCard getTrainCard(String gameName){
+
+        return new TrainCard(TrainColor.Black);
+    }
 }
