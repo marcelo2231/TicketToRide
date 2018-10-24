bdeda12807e6adfd0bcab7ebbc9d96da061140a6
adow2 <adow0331@gmail.com>
2018-10-24 11:58:52 -0600

    Removed discard pile from destination card deck

--- models/src/main/java/com/emmettito/models/Cards/DestinationCardDeck.java
+++ models/src/main/java/com/emmettito/models/Cards/DestinationCardDeck.java
@@ -1,39 +1,33 @@
 package com.emmettito.models.Cards;
 
+import java.util.Collections;
 import java.util.List;
 
 public class DestinationCardDeck implements Deck {
     private List<DestinationCard> availableCards;
-    private List<DestinationCard> discardPile;
+
 
     @Override
     public void shuffle() {
-
+        Collections.shuffle(availableCards);
     }
 
     @Override
     public void addCardToBottom(Card cardToAdd) {
-
+        DestinationCard card = (DestinationCard) cardToAdd;
+        availableCards.add(card);
     }
 
     public void setAvailableCards(List<DestinationCard> availableCards) {
         this.availableCards = availableCards;
     }
 
-    public void setDiscardPile(List<DestinationCard> discardPile) {
-        this.discardPile = discardPile;
-    }
-
     public List<DestinationCard> getAvailableCards() {
         return availableCards;
     }
 
-    public List<DestinationCard> getDiscardPile() {
-        return discardPile;
-    }
-
     @Override
-    public DestinationCard drawCard(){
+    public DestinationCard drawCard()throws Exception{
         //returns the first available card
         if(availableCards.get(0) != null){
             DestinationCard top = availableCards.get(0);
