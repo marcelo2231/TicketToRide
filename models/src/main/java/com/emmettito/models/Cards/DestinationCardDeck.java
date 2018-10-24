2aa5e90c37cdef83055d245e3a13701bcda6eda6
adow2 <adow0331@gmail.com>
2018-10-24 12:15:29 -0600

    Added the Card Interface so the two decks can inherit more methods

---
+++ models/src/main/java/com/emmettito/models/Cards/DestinationCardDeck.java
@@ -0,0 +1,47 @@
+package com.emmettito.models.Cards;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+public class DestinationCardDeck implements Deck {
+    private List<DestinationCard> availableCards;
+    private List<DestinationCard> discardPile;
+
+    public DestinationCardDeck(){
+        availableCards = new ArrayList<>();
+        discardPile = new ArrayList<>();
+
+        for(int i = 1; i <= 30; i++){
+            Random r = new Random();
+            int randomPointValue = r.nextInt(23) + 1;
+            availableCards.add(new DestinationCard("Provo", "Las Vegas", randomPointValue, i)); // TODO: Add cities and point values
+        }
+
+        shuffle();
+    }
+
+    @Override
+    public void shuffle() {
+        Collections.shuffle(availableCards);
+        Collections.shuffle(discardPile);
+    }
+
+    public void setAvailableCards(List<DestinationCard> availableCards) {
+        this.availableCards = availableCards;
+    }
+
+    public void setDiscardPile(List<DestinationCard> discardPile) {
+        this.discardPile = discardPile;
+    }
+
+    public List<DestinationCard> getAvailableCards() {
+        return availableCards;
+    }
+
+    public List<DestinationCard> getDiscardPile() {
+        return discardPile;
+    }
+
+}
