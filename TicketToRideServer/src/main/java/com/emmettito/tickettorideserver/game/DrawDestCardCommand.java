33e002d961f8813b4b5ebf81d0cbb9e3ca28b0fc
Marcelo Archiza <archiza@hotmail.com>
2018-10-31 12:57:20 -0600

    Get list of commands functionality

--- TicketToRideServer/src/main/java/com/emmettito/tickettorideserver/game/DrawDestCardCommand.java
+++ TicketToRideServer/src/main/java/com/emmettito/tickettorideserver/game/DrawDestCardCommand.java
@@ -38,6 +38,11 @@ public class DrawDestCardCommand implements IGameCommand{
         result.setSuccess(true);
         result.setData(card);
         result.setMessage("Successfully draw dest card.");
+
+        // Add to command list
+        String serializedRequest = new Serializer().serialize(commandModel);
+        String serializedResult = new Serializer().serialize(result);
+        gameDatabase.addCommand(commandModel.getGameName(), this.getClass(), serializedRequest, serializedResult);
         return result;
     }
 }
---
+++ TicketToRideServer/src/main/java/com/emmettito/tickettorideserver/game/GetCommandsCommand.java
@@ -0,0 +1,33 @@
+package com.emmettito.tickettorideserver.game;
+
+import com.emmettito.models.CommandModels.Commands;
+import com.emmettito.models.CommandModels.GameCommands.GetCommandsRequest;
+import com.emmettito.models.Results.GetCommandsResult;
+import com.emmettito.tickettorideserver.communication.Serializer;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+
+public class GetCommandsCommand implements IGameCommand {
+    GetCommandsRequest commandModel;
+
+    @Override
+    public GetCommandsResult execute(Object obj, String authToken) throws Exception {
+        /** Validate **/
+        try {
+            commandModel = (GetCommandsRequest) new Serializer().deserialize((InputStream) obj, GetCommandsRequest.class);
+        } catch (Exception e) {
+            throw new Exception("LoginCommand: command was on different format, please, make sure to set the LoginCommandModel.");
+        }
+
+        /** Get chat **/
+        GetCommandsResult result = new GetCommandsResult();
+        ArrayList<String> data = gameDatabase.getCommands(commandModel.getGameName(), commandModel.getCurrIndex());
+
+        /** Prepare Result **/
+        result.setSuccess(true);
+        result.setData(data);
+        result.setMessage("Successfully got commands.");
+        return result;
+    }
+}
