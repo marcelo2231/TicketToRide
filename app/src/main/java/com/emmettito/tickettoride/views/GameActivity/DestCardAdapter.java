c657b228072a57f2d5701cb19f6e540e103ff6ca
Jason <thecooljason@gmail.com>
2018-10-31 20:49:02 -0600

    Working on recycler for the dest card display

--- app/src/main/java/com/emmettito/tickettoride/views/GameActivity/DestCardAdapter.java
+++ app/src/main/java/com/emmettito/tickettoride/views/GameActivity/DestCardAdapter.java
@@ -1,6 +1,37 @@
 package com.emmettito.tickettoride.views.GameActivity;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class DestCardAdapter extends RecyclerView<DestCardAdapter> {
+
+public class DestCardAdapter extends RecyclerView.Adapter<DestCardAdapter.DestCardHolder> {
+    public class DestCardHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        @Override
+        public void onClick(View view) {
+
+        }
+
+        public DestCardHolder(View v) {
+            super(v);
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    @Override
+    public DestCardHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(DestCardHolder holder, int position) {
+
+    }
+
+
 }
