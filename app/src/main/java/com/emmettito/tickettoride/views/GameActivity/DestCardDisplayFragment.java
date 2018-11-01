86b598b4a9d5d0ac7006a442cf2964987535a92c
Jason <thecooljason@gmail.com>
2018-10-31 21:10:43 -0600

    Working on recycler for the dest card display

---
+++ app/src/main/java/com/emmettito/tickettoride/views/GameActivity/DestCardDisplayFragment.java
@@ -0,0 +1,147 @@
+package com.emmettito.tickettoride.views.GameActivity;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.emmettito.models.Cards.DestinationCard;
+import com.emmettito.tickettoride.Client;
+import com.emmettito.tickettoride.R;
+
+import java.util.List;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link DestCardDisplayFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link DestCardDisplayFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class DestCardDisplayFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private RecyclerView mRecyclerView;
+    private List<DestinationCard> destCards;
+    private Button mEndButton;
+
+    private OnFragmentInteractionListener mListener;
+
+    public DestCardDisplayFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment DestCardDisplayFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static DestCardDisplayFragment newInstance(String param1, String param2) {
+        DestCardDisplayFragment fragment = new DestCardDisplayFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        Client client = Client.getInstance();
+        destCards = client.getPlayerDestCards();
+
+        View view = inflater.inflate(R.layout.fragment_dest_card_display, container, false);
+        // Inflate the layout for this fragment
+        mRecyclerView = view.findViewById(R.id.dest_card_display_recycler);
+        LinearLayoutManager mgr = new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false);
+        mRecyclerView.setLayoutManager(mgr);
+        DestCardAdapter adapter = new DestCardAdapter(destCards);
+        mRecyclerView.setAdapter(adapter);
+
+        mEndButton = view.findViewById(R.id.display_dest_finish);
+        mEndButton.setEnabled(true);
+        mEndButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+        return view;
+    }
+
+    public void setDestCards(List<DestinationCard> cards) {
+        this.destCards = cards;
+    }
+
+    public void finish() {
+        getActivity().getSupportFragmentManager().beginTransaction().remove(this).commit();
+    }
+
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
