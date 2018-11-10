d07ffdae4137c2753614c0b39e5e71602079892b
JohnPaul <neutronstar@gmx.us>
2018-11-10 16:45:20 -0700

    Merge branch 'master' of https://github.com/marceloarchiza/TicketToRide
    # Conflicts:
    #	.idea/caches/build_file_checksums.ser

--- app/src/main/java/com/emmettito/tickettoride/views/GameActivity/GameHistoryActivity.java
+++ app/src/main/java/com/emmettito/tickettoride/views/GameActivity/GameHistoryActivity.java
@@ -6,8 +6,10 @@ import android.os.Handler;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 import com.emmettito.models.Results.GetCommandsResult;
 import com.emmettito.tickettoride.Client;
@@ -26,6 +28,8 @@ public class GameHistoryActivity extends FragmentActivity implements GameHistory
     private List<String[]> commands;
     private Client clientInstance = Client.getInstance();
     private String dataString = "";
+
+    private Button exitGameHistoryActivityButton;
 
     Handler timerHandler = new Handler();
     Runnable timerRunnable = new Runnable() {
@@ -63,6 +67,27 @@ public class GameHistoryActivity extends FragmentActivity implements GameHistory
         setContentView(R.layout.activity_game_history);
         //Toast.makeText(this, "Chat entered!", Toast.LENGTH_SHORT).show();
 
+        exitGameHistoryActivityButton = (Button) findViewById(R.id.exitgamehistorybutton);
+        exitGameHistoryActivityButton.setEnabled(true);
+        exitGameHistoryActivityButton.setOnClickListener(new View.OnClickListener() {
+            /**
+             * onClick
+             *
+             * This method defines the behavior of the click and is inherited from the
+             * OnClickListener parent class. This implementation finish the activity.
+             *
+             * @pre None
+             *
+             * @post chat activity is closed
+             *
+             */
+            public void onClick(View v) {
+                //Toast.makeText(getApplicationContext(), "You Can't Exit The Chat!", Toast.LENGTH_SHORT).show();
+                presenter.shutDownPoller();
+                finish();
+            }
+        });
+
         recycle = (RecyclerView) findViewById(R.id.my_recycler);
 
         recycle.setHasFixedSize(true);
@@ -82,9 +107,16 @@ public class GameHistoryActivity extends FragmentActivity implements GameHistory
     public void update(Object arg) {
         String newListString = (String) arg;
 
+        System.out.println("I got here");
+
+        System.out.println(dataString);
+        System.out.println(newListString);
+
         if (newListString.equals(dataString)) {
             return;
         }
+
+        System.out.println("This ias fsdf");
 
         dataString = newListString;
 
@@ -92,7 +124,9 @@ public class GameHistoryActivity extends FragmentActivity implements GameHistory
 
         List<String[]> commandsList = result.getData();
 
-        List<String[]> commandsStringsList = new ArrayList<>();
+        System.out.println(commandsList);
+
+        //List<String[]> commandsStringsList = new ArrayList<>();
 
         if (commandsList == null) {
             return;
@@ -100,7 +134,7 @@ public class GameHistoryActivity extends FragmentActivity implements GameHistory
 
         if (commandsList.size() > 0) {
             commands.clear();
-            commands.addAll(commandsStringsList);
+            commands.addAll(commandsList);
         }
     }
 
