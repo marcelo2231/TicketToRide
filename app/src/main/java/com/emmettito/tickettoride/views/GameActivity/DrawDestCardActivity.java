3ce953e435d600e9918d7e0005d53e9fc0a5a7c0
Jason <thecooljason@gmail.com>
2018-11-28 12:04:44 -0700

    Completed DrawDestCardActivity with and completely replaced the old fragment.

--- app/src/main/java/com/emmettito/tickettoride/views/GameActivity/DrawDestCardActivity.java
+++ app/src/main/java/com/emmettito/tickettoride/views/GameActivity/DrawDestCardActivity.java
@@ -2,10 +2,13 @@ package com.emmettito.tickettoride.views.GameActivity;
 
 
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.TextView;
@@ -55,6 +58,14 @@ public class DrawDestCardActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
         setContentView(R.layout.activity_draw_dest_card);
         Client client = Client.getInstance();
         this.mFirstTime = getIntent().getBooleanExtra("isFirst", false);
