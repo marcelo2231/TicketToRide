f18c3b3041965e5ee2ac17b4c744ca26ffeb6ded
Jason <thecooljason@gmail.com>
2018-12-07 10:07:44 -0700

    Working on SQLgameDAO

--- TicketToRideServer/src/main/java/com/emmettito/tickettorideserver/database/SQL/SQLGameDAO.java
+++ TicketToRideServer/src/main/java/com/emmettito/tickettorideserver/database/SQL/SQLGameDAO.java
@@ -3,14 +3,38 @@ package com.emmettito.tickettorideserver.database.SQL;
 import com.emmettito.models.CommandModels.Command;
 import com.emmettito.models.Game;
 import com.emmettito.tickettorideserver.database.IGameDAO;
+import com.google.gson.Gson;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class SQLGameDAO implements IGameDAO {
 
+    private Connection conn;
+
+    public SQLGameDAO(Connection conn) { this.conn = conn; };
+
+
     @Override
     public boolean addGame(Game game) {
-        return false;
+        PreparedStatement stmt = null;
+        String sql = "INSERT INTO `Games` VALUES (?,?);";
+        Gson gson = new Gson();
+        try {
+            stmt = conn.prepareStatement(sql);
+            stmt.setString(1, game.getGameName());
+            stmt.setString(2, gson.toJson(game));
+            stmt.execute();
+        }
+        catch (SQLException e) {
+            System.out.println(e.toString());
+            return false;
+        }
+
+        return true;
     }
 
     @Override
@@ -20,16 +44,54 @@ public class SQLGameDAO implements IGameDAO {
 
     @Override
     public Game getGame(String gameName) {
-        return null;
+        PreparedStatement stmt = null;
+        String sql = "SELECT * FROM `Games` WHERE GameID = ?;;";
+        ResultSet rs = null;
+        Gson gson = new Gson();
+        try {
+            stmt = conn.prepareStatement(sql);
+            stmt.setString(1, gameName);
+            rs = stmt.executeQuery();
+        }
+        catch (SQLException e) {
+            System.out.println(e.toString());
+        }
+        Game returnedGame = gson.fromJson(rs.toString(), Game.class);
+        return returnedGame;
     }
 
     @Override
     public boolean removeGame(String gameName) {
-        return false;
+        PreparedStatement stmt = null;
+        String sql = "DELETE from `Games` WHERE gameID = ?;";
+        Gson gson = new Gson();
+        try {
+            stmt = conn.prepareStatement(sql);
+            stmt.setString(1, gameName);
+            stmt.execute();
+        }
+        catch (SQLException e) {
+            System.out.println(e.toString());
+            return false;
+        }
+
+        return true;
     }
 
     @Override
     public Game updateGame(String gameName, List<Command> commands) {
+        PreparedStatement stmt = null;
+        String sql = "UPDATE `Games` SET Data = ? WHERE gameID = ?";
+        Gson gson = new Gson();
+        try {
+            stmt = conn.prepareStatement(sql);
+            stmt.setString(1, gameName);
+            stmt.execute();
+        }
+        catch (SQLException e) {
+            System.out.println(e.toString());
+        }
+
         return null;
     }
 
@@ -50,6 +112,15 @@ public class SQLGameDAO implements IGameDAO {
 
     @Override
     public boolean clearGames() {
+        PreparedStatement stmt = null;
+        String sql = "Delete from `Games`";
+        try {
+            stmt = conn.prepareStatement(sql);
+            stmt.execute();
+        }
+        catch (SQLException e) {
+            System.out.println(e.toString());
+        }
         return false;
     }
 
