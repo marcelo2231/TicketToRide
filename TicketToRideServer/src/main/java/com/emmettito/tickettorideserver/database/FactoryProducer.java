6563c691f3ba58260ef258fb27ef8300758196d8
Emmett <dgeough@gmail.com>
2018-12-06 18:00:22 -0700

    Changed isPlugin to getPlugin (NOTE: this does NOT load the plugin)

--- TicketToRideServer/src/main/java/com/emmettito/tickettorideserver/database/FactoryProducer.java
+++ TicketToRideServer/src/main/java/com/emmettito/tickettorideserver/database/FactoryProducer.java
@@ -6,8 +6,14 @@ public class FactoryProducer {
 
     String plugin_directory = System.getProperty("user.dir") + "/plugins";
 
-    public boolean isPlugin(String database_type) {
-        File[] files = new File(plugin_directory).listFiles();
+    public File getPlugin(String database_type) throws Exception {
+        File folder = new File(plugin_directory);
+
+        if (!folder.exists()) {
+            throw new Exception("Error: plugin folder does not exist");
+        }
+
+        File[] files = folder.listFiles();
 
         for (int i = 0; i < files.length; i++) {
             if (files[i].isFile()) {
@@ -15,21 +21,18 @@ public class FactoryProducer {
                 filename = filename.substring(0, filename.lastIndexOf('.')); // strips the file extension
 
                 if (filename.equals(database_type)) {
-                    return true;
+                    return files[i];
                 }
             }
         }
 
-        return false;
+        throw new Exception("Error: \"" + database_type + "\" plugin does not exist");
     }
 
-    public AbstractDAOFactory getFactory(String database_type) {
+    public AbstractDAOFactory getFactory(String database_type) throws Exception {
         database_type = database_type.toLowerCase();
 
-        if (isPlugin(database_type)) {
-            System.out.println(database_type);
-            return null;
-        }
+        File plugin = getPlugin(database_type); // TODO: needs to load plugin
 
 //        if (pluginType.equalsIgnoreCase("sqlite")) {
 //            return new SQLFactory();
