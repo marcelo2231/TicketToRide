be3550c15082648b6f5ddad4f69df462b1c5a890
JohnPaul <neutronstar@gmx.us>
2018-12-08 22:11:51 -0700

    Merge branch 'master' of https://github.com/marceloarchiza/TicketToRide
    # Conflicts:
    #	.idea/caches/build_file_checksums.ser

--- TicketToRideServer/src/main/java/com/emmettito/tickettorideserver/database/FlatFile/FFUserDAO.java
+++ TicketToRideServer/src/main/java/com/emmettito/tickettorideserver/database/FlatFile/FFUserDAO.java
@@ -4,22 +4,36 @@ import com.emmettito.models.User;
 import com.emmettito.tickettorideserver.communication.Serializer;
 import com.emmettito.tickettorideserver.database.IUserDAO;
 
+import org.apache.commons.io.FileUtils;
+
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 
 public class FFUserDAO implements IUserDAO {
     Serializer serializer = new Serializer();
 
+    String directory = System.getProperty("user.dir") + "/Users/";
+
+    FFUserDAO() {
+        new File(directory).mkdir();
+    }
+
     @Override
     public boolean addUser(String username, String password) {
-        String fileName = "Users/" + username;
+
+        String fileName = directory + username + ".txt";
         try {
             String gameJson = serializer.serialize(new User(username, password));
             PrintWriter out = new PrintWriter(fileName);
+            System.out.println(gameJson);
             out.println(gameJson);
+            out.close();
         }
         catch (Exception e){
+            e.printStackTrace();
             return false;
         }
         return true;
@@ -27,10 +41,16 @@ public class FFUserDAO implements IUserDAO {
 
     @Override
     public User getUser(String username) {
-        File[] files = new File("Users/").listFiles();
+        System.out.println(directory);
+        File[] files = new File(directory).listFiles();
+
         for (File f : files){
+            System.out.println(f.getName());
             try{
-                User user = (User)serializer.deserialize(new ByteArrayInputStream(f.toString().getBytes()), User.class);
+                String content = new Scanner(f).useDelimiter("\\Z").next();
+
+                User user = (User)serializer.deserialize(new ByteArrayInputStream(content.getBytes(StandardCharsets.UTF_8)), User.class);
+
                 if(user.getUsername().equals(username)){
                     return user;
                 }
@@ -52,11 +72,13 @@ public class FFUserDAO implements IUserDAO {
 
     @Override
     public void clearDatabase() {
-        File dir = new File("Users");
+        File dir = new File(directory);
         try {
-            //FileUtils.deleteDirectory(dir);
+            FileUtils.deleteDirectory(dir);
         }
         catch (Exception e){
         }
+
+        dir.mkdir();
     }
 }
